fe44f52b3f96e768ba065dcc26221233495c944d
Cedrik Vanderhaegen <cedrik.vanderhaegen@geosolutions.be>
2015-09-10 09:57:49 +0200

    add package.js

---
+++ package.js
@@ -0,0 +1,22 @@
+var miniExcludes = {
+	},
+	amdExcludes = {
+	},
+	isJsRe = /\.js$/,
+	isTestRe = /\/test\//;
+
+var profile = {
+	resourceTags: {
+		test: function(filename, mid){
+			return isTestRe.test(filename);
+		},
+
+		miniExclude: function(filename, mid){
+			return isTestRe.test(filename) || mid in miniExcludes;
+		},
+
+		amd: function(filename, mid){
+			return isJsRe.test(filename) && !(mid in amdExcludes);
+		}
+	}
+};
